1d2fff37ae9d279812adf0ddbe5316f157ec9316
kev200 <bleedeagle@gmail.com>
2021-08-12 12:46:23 +0300

    Update multicity_custom_js.js

--- WCD/multicity_custom_js.js
+++ WCD/multicity_custom_js.js
@@ -1,4 +1,6 @@
 
+$(document).ready(function() {
+
 /* Change Slick slider arrows (Dependancy Slick JS)*/
 $("#blog_slides").slick({
     prevArrow:"<img class='a-left control-c prev slick-prev' src='https://uploads-ssl.webflow.com/60ae9a8dbba8f536b22321fb/60e1aa219137b92dafa93e44_Previous2.svg'>",
@@ -8,7 +10,7 @@ $("#blog_slides").slick({
 
 
 /* Change Slider numberical slide value based on the slide active (Dependancy attrchange.min.js)*/  
-$(document).ready(function() {
+
     /* 1. get the number of slides */
     var numItems = $(".w-slider-dot").length;
     /* format total slides like this: 05...09...10....12"*/
@@ -26,7 +28,7 @@ $(document).ready(function() {
         document.getElementById("counter").innerHTML =  (myIndex < 9 ? '0' : '') + (myIndex +1)
         }
     });
-});  
+ 
 
 /* Change Flipster behavior (dependancy Flipster) */
 
@@ -44,95 +46,11 @@ var coverflow = $("#coverflow").flipster({
     start: 'center',
  });
 
-  /*!
-* jquery.counterup.js 1.0
-*
-* Copyright 2013, Benjamin Intal http://gambit.ph @bfintal
-* Released under the GPL v2 License
-*
-* Date: Nov 26, 2013
-*/
-(function( $ ){
-    "use strict";
-  
-    $.fn.counterUp = function( options ) {
-  
-      // Defaults
-      var settings = $.extend({
-          'time': 400,
-          'delay': 10
-      }, options);
-  
-      return this.each(function(){
-  
-          // Store the object
-          var $this = $(this);
-          var $settings = settings;
-            var origValue = $this.text();
-  
-          var counterUpper = function() {
-              var nums = [];
-              var divisions = $settings.time / $settings.delay;
-              var num = origValue;
-              var isComma = /[0-9]+,[0-9]+/.test(num);
-              num = num.replace(/,/g, '');
-              var isInt = /^[0-9]+$/.test(num);
-              var isFloat = /^[0-9]+\.[0-9]+$/.test(num);
-              var decimalPlaces = isFloat ? (num.split('.')[1] || []).length : 0;
-  
-              // Generate list of incremental numbers to display
-              for (var i = divisions; i >= 1; i--) {
-  
-                  // Preserve as int if input was int
-                  var newNum = parseInt(num / divisions * i);
-  
-                  // Preserve float if input was float
-                  if (isFloat) {
-                      newNum = parseFloat(num / divisions * i).toFixed(decimalPlaces);
-                  }
-  
-                  // Preserve commas if input had commas
-                  if (isComma) {
-                      while (/(\d+)(\d{3})/.test(newNum.toString())) {
-                          newNum = newNum.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
-                      }
-                  }
-  
-                  nums.unshift(newNum);
-              }
-  
-              $this.data('counterup-nums', nums);
-              $this.text('0');
-  
-              // Updates the number until we're done
-              var f = function() {
-                  $this.text($this.data('counterup-nums').shift());
-                  if ($this.data('counterup-nums').length) {
-                      setTimeout($this.data('counterup-func'), $settings.delay);
-                  } else {
-                      delete $this.data('counterup-nums');
-                      $this.data('counterup-nums', null);
-                      $this.data('counterup-func', null);
-                  }
-              };
-              $this.data('counterup-func', f);
-  
-              // Start the count up
-              setTimeout($this.data('counterup-func'), $settings.delay);
-          };
-  
-          // Perform counts when the element gets into view
-          $this.waypoint(counterUpper, { offset: '100%', triggerOnce: true });
-      });
-  
-    };
-  
-  })( jQuery );
+$('.counter').counterUp({
+    delay: 100, // the delay time in ms
+    time: 2000 // the speed time in ms
+});
 
 
-jQuery(document).ready(function( $ ) {
-    $('.counter').counterUp({
-        delay: 100, // the delay time in ms
-        time: 2000 // the speed time in ms
-    });
+
 });
