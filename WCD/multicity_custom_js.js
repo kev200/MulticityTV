b6035dcc98ba9b3768e7fa732317afad8440896d
kev200 <bleedeagle@gmail.com>
2021-09-06 10:32:32 +0300

    Update multicity_custom_js.js

--- WCD/multicity_custom_js.js
+++ WCD/multicity_custom_js.js
@@ -104,24 +104,21 @@ $('.slick-carousel-container').slick({
 });
 
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
+    slidesPerView: 3,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
     pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
+        el: ".swiper-pagination",
+        clickable: true,
     },
   });
 
